9dfc9544460f8e9d06c1ad767c8a8b648040adfa
davidsan13 <sandavid08@yahoo.com>
2022-07-10 14:25:40 -0700

    search function for bst

--- dataStructure/binarySearchTree.js
+++ dataStructure/binarySearchTree.js
@@ -22,7 +22,6 @@ class bst {
     };
 
     insertNode(node, newNode) {
-      console.log(node.value)
       if (newNode.value < node.value) {
         if (node.left === null)
           node.left = newNode;
@@ -39,6 +38,17 @@ class bst {
           this.insertNode(node.right, newNode);
       }
     };
+
+    search(root, key){
+      if(root == null || root.value == key)
+        return root;
+
+      if(root.value > key){
+        return(root.right, key)
+      }else {
+        return(root.left, key)
+      }
+    };
   }
 // }
 
@@ -48,3 +58,5 @@ p1.insert(4)
 p1.insert(3)
 p1.insert(8)
 console.log(p1)
+
+console.log(p1.search(4, 8));

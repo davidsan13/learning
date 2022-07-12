a63fdb72798ba322345ce244126273c728429cd3
davidsan13 <sandavid08@yahoo.com>
2022-07-11 21:01:30 -0700

    getRootNode, countNode, getMax, findHeight of binary search implemented

--- dataStructure/binarySearchTree.js
+++ dataStructure/binarySearchTree.js
@@ -5,6 +5,7 @@ class Node {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.height = null;
   }
 }
 
@@ -40,23 +41,58 @@ class bst {
     };
 
     search(root, key){
-      if(root == null || root.value == key)
+      if(root == null || root.value == key) {
         return root;
-
-      if(root.value > key){
-        return(root.right, key)
-      }else {
-        return(root.left, key)
       }
+
+      if(root.value < key){
+        return this.search(root.right, key)
+      }else {
+        return this.search(root.left, key)
+      }
+    };
+
+    getRootNode() {
+      return this.root
+    }
+
+    countNode(root) {
+      if(root == null)
+        return 0
+      
+      return 1 + this.countNode(root.left) + this.countNode(root.right)
+    }
+
+    getMax(root) {
+      if(root.right === null)
+        return root
+      return this.getMax(root.right)
+    };
+
+    findHeight(root) {
+      if (root === null)
+        return -1
+
+      let leftHeight = this.findHeight(root.left)
+      let rightHeight = this.findHeight(root.right)
+      root.height = Math.max(leftHeight, rightHeight) + 1;
+      return Math.max(leftHeight, rightHeight) + 1
     };
   }
 // }
 
 let p1 = new bst();
-console.log(p1);
+
 p1.insert(4)
 p1.insert(3)
 p1.insert(8)
-console.log(p1)
+p1.insert(6)
+p1.insert(7)
+// p1.insert(10)
+// p1.insert(15)
+// p1.insert(20)
+let root = p1.getRootNode()
+let root2 = p1.search(root, 8)
+console.log(p1.findHeight(root))
+console.log(root)
 
-console.log(p1.search(4, 8));

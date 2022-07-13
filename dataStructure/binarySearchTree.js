c95960ac5a5e0a4fb5f28ad867b630771e5d2615
davidsan13 <sandavid08@yahoo.com>
2022-07-12 22:21:41 -0700

    removeNode bst

--- dataStructure/binarySearchTree.js
+++ dataStructure/binarySearchTree.js
@@ -69,6 +69,12 @@ class bst {
       return this.getMax(root.right)
     };
 
+    findMin(root) {
+      if(root.left === null)
+        return root
+      return this.findMin(root.left)
+    };
+
     findHeight(root) {
       if (root === null)
         return -1
@@ -101,8 +107,67 @@ class bst {
       this.preorder(root.left);
       console.log(root.value)
       this.preorder(root.right);
+    };
+
+    isBinarySearchTree(root) {
+      if(root == null) return true;
+      if(this.isSubtreeLesser(root.left, root.value)
+        && this.isSubtreeGreater(root.right, root.value)
+        && this.isBinarySearchTree(root.left)
+        && this.isBinarySearchTree(root.left))
+        return true;
+      else
+        return false;
     }
 
+    isSubtreeLesser(root, value) {
+      if(root == null) return true;
+      if(root.value <= value 
+        && this.isSubtreeLesser(root.left, value)
+        && this.isSubtreeLesser(root.right, value))
+        return true;
+      else
+        return false;
+    };
+
+    isSubtreeGreater(root, value) {
+      if(root == null) return true;
+      if(root.value > value 
+        && this.isSubtreeGreater(root.left, value)
+        && this.isSubtreeGreater(root.right, value))
+        return true;
+      else
+        return false;
+    };
+
+    removeNode(root, value) {
+      if(root == null) return root;
+      else if(value < root.value) {
+        root.left = this.removeNode(root.left, value)
+      } else if(data > root.value) root.right = this.removeNode(root.right, value);
+      else {
+        if(root.left == null && root.right == null) {
+          delete root;
+          root = null;
+        }
+        else if(root.left == null) {
+          let temp = new Node(root)
+          root = root.right;
+          delete temp;
+        }
+        else if(root.right == null) {
+          let temp = new Node(root);
+          root = root.left;
+          delete temp
+        }
+        else {
+          let temp = this.findMin(root.right)
+          root.value = temp.value;
+          root.right = this.removeNode(root.right, temp.value)
+        }
+      }
+      return root
+    }
   }
 // }
 
@@ -120,5 +185,5 @@ let root = p1.getRootNode()
 let root2 = p1.search(root, 8)
 // console.log(p1.findHeight(root))
 console.log(root)
-p1.preorder(root);
+console.log(p1.isBinarySearchTree(root));
 
